1526a24e9c343b3efa0503893a6b412c708183d1
Rishitaa22 <rishita.2028@cs.sjcetpalai.ac.in>
2024-10-01 15:21:53 +0530

    Add files via upload

---
+++ student details1.py
@@ -0,0 +1,17 @@
+'''
+Author: Rishita
+Date: 01-10-24
+Version: 1.0
+Python program to get the student details
+'''
+
+print("hello")
+name=input("enter the student name:")
+roll_number=input("enter your roll number:")
+print("Name of student:",name)
+print("Student roll number:",roll_number)
+cgpa=float(input("enter student cgpa:"))
+percentage=("enter percentage of student:")
+Percentage_of_student=cgpa*10
+print("Cgpa of student:",cgpa)
+print("percentage of student:",percentage)
