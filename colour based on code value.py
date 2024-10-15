81f4cb6bd85d921a164ac86878cc6ec7e83507a3
Rishitaa22 <rishita.2028@cs.sjcetpalai.ac.in>
2024-10-15 15:14:00 +0530

    Add files via upload

---
+++ colour based on code value.py
@@ -0,0 +1,16 @@
+'''
+Author: Rishita
+Date: 15-10-2024
+Python program to determine colour based on a given code value.
+version 1.1
+'''
+grade=input("Enter grade:")
+print("grade:",grade)
+if grade=="R":
+    print("Red")
+elif grade=="G":
+    print("Green")
+elif grade=="B":
+    print("Blue")
+else:
+    print("Invalid code")
