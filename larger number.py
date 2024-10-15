a6c7e8075a22cded693fd4cf1443c9a93a53a3ad
Rishitaa22 <rishita.2028@cs.sjcetpalai.ac.in>
2024-10-15 14:43:56 +0530

    Add files via upload
    program to determine the larger number of two numbers

---
+++ larger number.py
@@ -0,0 +1,13 @@
+'''
+Author: Rishita
+Date: 15-10-2024
+program to determine the larger number of two numbers.
+version 1.1
+'''
+
+num1=int(input("Enter a number 1:"))
+num2=int(input("Enter a number 2:"))
+if num1>num2:
+    print("number 1 is greater than number 2")
+else:
+    print("number 2 is greater than number 1")
