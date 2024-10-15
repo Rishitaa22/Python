0e4a4bfdc7045c97255fc793f564cd3deabcde29
Rishitaa22 <rishita.2028@cs.sjcetpalai.ac.in>
2024-10-15 14:32:23 +0530

    Add files via upload
    program to find simple interest

---
+++ simple interest.py
@@ -0,0 +1,12 @@
+'''
+Author: Rishita
+Date: 15-10-2024
+program to find simple interest
+version 1.1
+'''
+
+principle=int(input("Enter principle amount:"))
+rate=int(input("Enter rate amount:"))
+year=int(input("Enter year:"))
+simple_interest=(principle*rate*year)
+print(simple_interest)
