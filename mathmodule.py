872d3272af0bbe1c2a99a39a0c744c5511761997
Rishitaa22 <rishita.2028@cs.sjcetpalai.ac.in>
2024-10-22 15:22:16 +0530

    Add files via upload

---
+++ mathmodule.py
@@ -0,0 +1,12 @@
+'''
+Author: Rishita
+Date: 07-10-2024
+Python program that uses functions from the math module to perform the following operations on a number provided by the user
+version 1.1
+'''
+
+import math
+num=int(input("Enter a number:"))
+print=("Square root of",num,":", math.sqrt(num))
+print=("Factorial of",num,":", math.factorial(num))
+print=(num,"raised to power of 2:", math.pow(num,2))
