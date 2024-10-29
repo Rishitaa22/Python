9b1e8c335433612367cecc5ee0d3209ccedc370e
Rishitaa22 <rishita.2028@cs.sjcetpalai.ac.in>
2024-10-29 16:10:40 +0530

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,12 @@
+'''
+Rishita
+Date:29/10/24
+Python program to check whether the entered number is odd or even.
+version 1
+'''
+number=int(input("Enter a positive number:"))
+print("number=",number)
+if (number%2==0):
+    print("The number is even.")
+else:
+    print("The number is odd.")
