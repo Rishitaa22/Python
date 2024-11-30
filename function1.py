817c2ccb552e1af06b1d38ac78a4a0d76ab343af
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-30 10:09:04 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,16 @@
+'''
+Author: Rishita
+Date: 30/11/2024
+Python program to find sum of numbers using functions.
+Version 1.0
+'''
+def add_two_numbers():
+    num1=10
+    num2=20
+    sum=num1+num2
+    print(sum)
+add_two_numbers()
+def add_two_numbers(num_1,num_2,num_3):
+    sum=num_1+num_2+num_3
+    print(sum)
+add_two_numbers(3,5,7)
